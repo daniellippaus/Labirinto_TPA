a5ea15d7e09aa264181f8edbd6487e629766b2c7
daniellippaus <86384529+daniellippaus@users.noreply.github.com>
2024-10-08 08:23:20 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insira o caminho do arquivo CSV: ");
+        String caminhoArquivo = scanner.nextLine(); // Lê o caminho do arquivo fornecido pelo usuário
+
         try {
             // Lê o arquivo CSV e carrega o labirinto
-            int[][] labirinto = LeitorCSV.lerArquivoCSV("src/labirinto_50x50.csv");
+            int[][] labirinto = LeitorCSV.lerArquivoCSV(caminhoArquivo);
 
             // Cria uma instância da classe Labirinto
             Labirinto lab = new Labirinto(labirinto);
@@ -14,6 +20,8 @@ public class Main {
 
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        } finally {
+            scanner.close(); // Fecha o scanner para evitar vazamentos de recursos
         }
     }
 }
